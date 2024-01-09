c90d1d4e8d43b8a14df7195dc6f79c5c555ea407
akash gupta <akash@AkashGupta>
2024-01-10 00:30:23 +0530

    First Backend Commit

---
+++ TaskModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const TaskSchema = new mongoose.Schema({
+    task:{
+        type:String,
+        require:true
+    },
+    des:{
+        type:String,
+        
+    }
+})
+
+const task = mongoose.model('task', TaskSchema)
+module.exports = task;
